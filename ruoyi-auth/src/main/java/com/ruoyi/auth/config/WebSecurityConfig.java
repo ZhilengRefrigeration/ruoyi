de0c51f5a78429ecb4e59d854ac9412c0d098b13
yuchao1990 <757143422@qq.com>
2020-06-15 14:38:27 +0800

    去除无用代码

--- ruoyi-auth/src/main/java/com/ruoyi/auth/config/WebSecurityConfig.java
+++ ruoyi-auth/src/main/java/com/ruoyi/auth/config/WebSecurityConfig.java
@@ -1,30 +1,27 @@
 package com.ruoyi.auth.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Security 安全认证相关配置
  * Oauth2依赖于Security 默认情况下WebSecurityConfig执行比ResourceServerConfig优先
- * 
+ *  PasswordEncoder AuthenticationManager 在 AuthServerConfig 中有用到
  * @author ruoyi
  */
 @Order(99)
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 {
-    @Autowired
-    private UserDetailsService userDetailsService;
+//    @Autowired
+//    private UserDetailsService userDetailsService;
 
+    // Spring的@Bean注解用于告诉方法，产生一个Bean对象，然后这个Bean对象交给Spring管理。产生这个Bean对象的方法Spring只会调用一次，随后这个Spring将会将这个Bean对象放在自己的IOC容器中。
     @Bean
     public PasswordEncoder passwordEncoder()
     {
@@ -38,22 +35,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
         return super.authenticationManagerBean();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception
-    {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+//    {
+//        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+//    }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception
-    {
-        http
-        .authorizeRequests()
-        .antMatchers(
-            "/actuator/**",
-            "/oauth/*",
-            "/token/**").permitAll()
-        .anyRequest().authenticated()
-        .and().csrf().disable();
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception
+//    {
+//        http
+//        .authorizeRequests()
+//        .antMatchers(
+//            "/actuator/**",
+//            "/oauth/*",
+//            "/token/**").permitAll()
+//        .anyRequest().authenticated()
+//        .and().csrf().disable();
+//    }
 }
