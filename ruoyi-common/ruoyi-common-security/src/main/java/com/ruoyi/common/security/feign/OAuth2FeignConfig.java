dcf22e61967f9c055877a9c94bfaad902f7a74fe
坎坎坷坷 <gyv12345@163.com>
2020-07-07 15:06:31 +0800

    简化代码

--- ruoyi-common/ruoyi-common-security/src/main/java/com/ruoyi/common/security/feign/OAuth2FeignConfig.java
+++ ruoyi-common/ruoyi-common-security/src/main/java/com/ruoyi/common/security/feign/OAuth2FeignConfig.java
@@ -1,8 +1,14 @@
 package com.ruoyi.common.security.feign;
 
+import com.ruoyi.common.core.constant.SecurityConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import feign.RequestInterceptor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 
 /**
  * Feign配置注册
@@ -10,11 +16,17 @@ import feign.RequestInterceptor;
  * @author ruoyi
  **/
 @Configuration
-public class OAuth2FeignConfig
-{
+public class OAuth2FeignConfig {
     @Bean
-    public RequestInterceptor requestInterceptor()
-    {
-        return new OAuth2FeignRequestInterceptor();
+    public RequestInterceptor requestInterceptor() {
+        return (requestTemplate) -> {
+            SecurityContext securityContext = SecurityContextHolder.getContext();
+            Authentication authentication = securityContext.getAuthentication();
+            if (authentication != null && authentication.getDetails() instanceof OAuth2AuthenticationDetails) {
+                OAuth2AuthenticationDetails dateils = (OAuth2AuthenticationDetails) authentication.getDetails();
+                requestTemplate.header(HttpHeaders.AUTHORIZATION,
+                        String.format("%s %s", SecurityConstants.BEARER_TOKEN_TYPE, dateils.getTokenValue()));
+            }
+        };
     }
 }
--- ruoyi-common/ruoyi-common-swagger/src/main/java/com/ruoyi/common/swagger/annotation/EnableCustomSwagger2.java
+++ ruoyi-common/ruoyi-common-swagger/src/main/java/com/ruoyi/common/swagger/annotation/EnableCustomSwagger2.java
@@ -9,6 +9,10 @@ import java.lang.annotation.Target;
 import org.springframework.context.annotation.Import;
 import com.ruoyi.common.swagger.config.SwaggerAutoConfiguration;
 
+/**
+ * 这个注解应该说没啥用处，因为在引入这个starter以后，就已经会去加载swagger
+ * 再多写一个注解根本就没用，只能通过配置文件设置是否使用swagger
+ */
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
