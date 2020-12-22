5e991c61081bda5a6840f672f6977f6a84c0e6fe
陈云志 <yunzhic@gridnt.com>
2020-12-22 17:31:42 +0800

    runing

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -28,13 +28,17 @@ module.exports = {
   productionSourceMap: false,
   // webpack-dev-server 相关配置
   devServer: {
+    allowedHosts: [
+      '192.168.8.13',
+      '192.168.8.201',
+    ],
     host: '0.0.0.0',
     port: port,
     open: true,
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8080`,
+        target: `http://192.168.8.201:8080`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
