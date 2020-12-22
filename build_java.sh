e5aa1b06eeade5ef6b88d1c87b32c38a765935d1
陈云志 <yunzhic@gridnt.com>
2020-12-22 18:12:45 +0800

    runing

--- build_java.sh
+++ build_java.sh
@@ -4,9 +4,17 @@
 
 for gfile in $(find . -name "build.gradle")
 do
-	
-    gradle -b $gfile clean build -x test -DbuildProduct=true
-    echo -e "\033[32m----------------- $gfile 模块编译成功 ---------------------- \033[0m"
+    if [[ $1='cl' ]];then 
+        
+        gradle -b $gfile clean;  
+        echo -e "\033[32m----------------- $gfile 模块 清理 成功 ---------------------- \033[0m"
+    else
+        gradle -b $gfile clean build -x test -DbuildProduct=true -DreleaseVersion=$1
+        echo -e "\033[32m----------------- $gfile 模块编译成功 ---------------------- \033[0m"
+    fi
+  
+    
 done
 
+ 
  
