58a2dcd17912cc9c5bce8e2e2009798e4bb837ba
陈云志 <yunzhic@gridnt.com>
2020-12-23 18:38:38 +0800

    发布依赖

--- build-config/build-java.sh
+++ build-config/build-java.sh
@@ -10,6 +10,11 @@ do
         echo -e "\033[32m----------------- $gfile 模块 清理 成功 ---------------------- \033[0m"
     else
         gradle -b $gfile clean build -x test -DbuildProduct=true -DreleaseVersion=$1 
+        
+        if [ 0 -ne $? ] ; then 
+	       echo -e "\033[31m-----------------$gfile 模块编译失败导致产品编译失败! ------------ \033[0m";
+	       continue; 
+        fi
         echo -e "\033[32m----------------- $gfile 模块编译成功 ---------------------- \033[0m"
     fi
   
