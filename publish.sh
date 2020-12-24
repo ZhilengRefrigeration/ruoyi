b38365fac43aed218ad88f1d050be1955eab0bc9
陈云志 <yunzhic@gridnt.com>
2020-12-24 13:13:04 +0800

    boot jar 支持ruoyi 依赖传递

--- publish.sh
+++ publish.sh
@@ -7,21 +7,21 @@ gradleArgs="clean publish -DbuildProduct=true -DreleaseVersion=$1"
 
 
 # 发布公共模块，必须按照下列顺序 编译发布。
-gradle $gradleArgs -b ./ruoyi-common/ruoyi-common-core/build.gradle
-gradle $gradleArgs -b ./ruoyi-api/ruoyi-api-system/build.gradle 
-gradle $gradleArgs -b ./ruoyi-common/ruoyi-common-redis/build.gradle 
-gradle $gradleArgs -b ./ruoyi-common/ruoyi-common-security/build.gradle 
-gradle $gradleArgs -b ./ruoyi-common/ruoyi-common-log/build.gradle 
-gradle $gradleArgs -b ./ruoyi-common/ruoyi-common-datascope/build.gradle 
-gradle $gradleArgs -b ./ruoyi-common/ruoyi-common-swagger/build.gradle 
+gradle $gradleArgs -b ./ruoyi-common/ruoyi-common-core/build.gradle -s
+gradle $gradleArgs -b ./ruoyi-api/ruoyi-api-system/build.gradle -s 
+gradle $gradleArgs -b ./ruoyi-common/ruoyi-common-redis/build.gradle -s 
+gradle $gradleArgs -b ./ruoyi-common/ruoyi-common-security/build.gradle -s 
+gradle $gradleArgs -b ./ruoyi-common/ruoyi-common-log/build.gradle -s 
+gradle $gradleArgs -b ./ruoyi-common/ruoyi-common-datascope/build.gradle -s 
+gradle $gradleArgs -b ./ruoyi-common/ruoyi-common-swagger/build.gradle -s 
 
 
 ### 六个 微服务依赖， monitor 不需要发布。
-gradle $gradleArgs -b ./ruoyi-auth/build.gradle 
-gradle $gradleArgs -b ./ruoyi-gateway/build.gradle 
-gradle $gradleArgs -b ./ruoyi-modules/ruoyi-system/build.gradle 
-gradle $gradleArgs -b ./ruoyi-modules/ruoyi-file/build.gradle
-gradle $gradleArgs -b ./ruoyi-modules/ruoyi-gen/build.gradle 
-gradle $gradleArgs -b ./ruoyi-modules/ruoyi-job/build.gradle 
+gradle $gradleArgs -b ./ruoyi-auth/build.gradle -s 
+gradle $gradleArgs -b ./ruoyi-gateway/build.gradle -s 
+gradle $gradleArgs -b ./ruoyi-modules/ruoyi-system/build.gradle -s 
+gradle $gradleArgs -b ./ruoyi-modules/ruoyi-file/build.gradle -s
+gradle $gradleArgs -b ./ruoyi-modules/ruoyi-gen/build.gradle -s 
+gradle $gradleArgs -b ./ruoyi-modules/ruoyi-job/build.gradle -s 
 
 
