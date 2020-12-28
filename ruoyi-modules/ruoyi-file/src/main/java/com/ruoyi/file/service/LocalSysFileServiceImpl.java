d0e85a00f813587cabce7bc70ca11b21d13874f7
陈云志 <yunzhic@gridnt.com>
2020-12-28 17:28:54 +0800

    文件上传 moni

--- ruoyi-modules/ruoyi-file/src/main/java/com/ruoyi/file/service/LocalSysFileServiceImpl.java
+++ ruoyi-modules/ruoyi-file/src/main/java/com/ruoyi/file/service/LocalSysFileServiceImpl.java
@@ -1,20 +1,18 @@
 package com.ruoyi.file.service;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 import com.ruoyi.file.utils.FileUploadUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * 本地文件存储
- * 
+ *
  * @author ruoyi
  */
-@Primary
-@Service
-public class LocalSysFileServiceImpl implements ISysFileService
-{
+
+//@Service
+//@ConfigurationProperties(prefix = "fdfs")
+public class LocalSysFileServiceImpl implements ISysFileService {
     /**
      * 资源映射路径 前缀
      */
@@ -26,7 +24,7 @@ public class LocalSysFileServiceImpl implements ISysFileService
      */
     @Value("${file.domain}")
     public String domain;
-    
+
     /**
      * 上传文件存储在本地的根路径
      */
@@ -35,13 +33,13 @@ public class LocalSysFileServiceImpl implements ISysFileService
 
     /**
      * 本地文件上传接口
-     * 
+     *
      * @param file 上传的文件
      * @return 访问地址
      * @throws Exception
      */
-    public String uploadFile(MultipartFile file) throws Exception
-    {
+    @Override
+    public String uploadFile(MultipartFile file) throws Exception {
         String name = FileUploadUtils.upload(localFilePath, file);
         String url = domain + localFilePrefix + name;
         return url;
