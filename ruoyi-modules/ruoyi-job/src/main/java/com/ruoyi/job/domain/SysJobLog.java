d7e6220d64c307f456519a1ffe108863db3687a0
anweiguo <anweiguo@ysujc.com>
2021-05-25 13:57:04 +0800

    支持mybatis-plus

--- ruoyi-modules/ruoyi-job/src/main/java/com/ruoyi/job/domain/SysJobLog.java
+++ ruoyi-modules/ruoyi-job/src/main/java/com/ruoyi/job/domain/SysJobLog.java
@@ -1,8 +1,15 @@
 package com.ruoyi.job.domain;
 
+import java.io.Serializable;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.baomidou.mybatisplus.annotation.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import com.ruoyi.common.core.annotation.Excel;
@@ -10,59 +17,124 @@ import com.ruoyi.common.core.web.domain.BaseEntity;
 
 /**
  * 定时任务调度日志表 sys_job_log
- * 
+ *
  * @author ruoyi
  */
 @Data
-public class SysJobLog extends BaseEntity
-{
+@NoArgsConstructor
+@Accessors(chain = true)
+@TableName("sys_job_log")
+public class SysJobLog implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    /** ID */
+    /**
+     * ID
+     */
     @Excel(name = "日志序号")
+    @TableId(value = "job_log_id", type = IdType.AUTO)
     private Long jobLogId;
 
-    /** 任务名称 */
+    /**
+     * 任务名称
+     */
     @Excel(name = "任务名称")
     private String jobName;
 
-    /** 任务组名 */
+    /**
+     * 任务组名
+     */
     @Excel(name = "任务组名")
     private String jobGroup;
 
-    /** 调用目标字符串 */
+    /**
+     * 调用目标字符串
+     */
     @Excel(name = "调用目标字符串")
     private String invokeTarget;
 
-    /** 日志信息 */
+    /**
+     * 日志信息
+     */
     @Excel(name = "日志信息")
     private String jobMessage;
 
-    /** 执行状态（0正常 1失败） */
+    /**
+     * 执行状态（0正常 1失败）
+     */
     @Excel(name = "执行状态", readConverterExp = "0=正常,1=失败")
     private String status;
 
-    /** 异常信息 */
+    /**
+     * 异常信息
+     */
     @Excel(name = "异常信息")
     private String exceptionInfo;
 
-    /** 开始时间 */
+    /**
+     * 开始时间
+     */
     private Date startTime;
 
-    /** 停止时间 */
+    /**
+     * 停止时间
+     */
     private Date stopTime;
+
+    /**
+     * 创建者
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private String createBy;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+
+    /**
+     * 更新者
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private String updateBy;
+
+    /**
+     * 更新时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date updateTime;
+
+    /**
+     * 备注
+     */
+    private String remark;
+
+    /**
+     * 搜索值
+     */
+    @TableField(exist = false)
+    private String searchValue;
+
+    /**
+     * 请求参数
+     */
+    @TableField(exist = false)
+    private Map<String, Object> params = new HashMap<>();
+
 
     @Override
     public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("jobLogId", getJobLogId())
-            .append("jobName", getJobName())
-            .append("jobGroup", getJobGroup())
-            .append("jobMessage", getJobMessage())
-            .append("status", getStatus())
-            .append("exceptionInfo", getExceptionInfo())
-            .append("startTime", getStartTime())
-            .append("stopTime", getStopTime())
-            .toString();
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("jobLogId", getJobLogId())
+                .append("jobName", getJobName())
+                .append("jobGroup", getJobGroup())
+                .append("jobMessage", getJobMessage())
+                .append("status", getStatus())
+                .append("exceptionInfo", getExceptionInfo())
+                .append("startTime", getStartTime())
+                .append("stopTime", getStopTime())
+                .toString();
     }
 }
