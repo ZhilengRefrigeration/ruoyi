e7d1fd6dbe51cd57c1557de483b725d8c354a8a8
lic <xwaf_love@yeah.net>
2021-05-30 17:44:13 +0800

    修复1处存在SQL注入漏洞问题，再实体类中移除

---
+++ ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/constant/DataScopeConstants.java
@@ -0,0 +1,39 @@
+package com.ruoyi.common.core.constant;
+
+/**
+ * 数据过滤常量
+ * 
+ * @author lic
+ */
+public class DataScopeConstants
+{
+    /**
+     * 全部数据权限
+     */
+    public static final String DATA_SCOPE_ALL = "1";
+
+    /**
+     * 自定数据权限
+     */
+    public static final String DATA_SCOPE_CUSTOM = "2";
+
+    /**
+     * 部门数据权限
+     */
+    public static final String DATA_SCOPE_DEPT = "3";
+
+    /**
+     * 部门及以下数据权限
+     */
+    public static final String DATA_SCOPE_DEPT_AND_CHILD = "4";
+
+    /**
+     * 仅本人数据权限
+     */
+    public static final String DATA_SCOPE_SELF = "5";
+
+    /**
+     * 数据权限过滤关键字
+     */
+    public static final String DATA_SCOPE = "dataScope";
+}
