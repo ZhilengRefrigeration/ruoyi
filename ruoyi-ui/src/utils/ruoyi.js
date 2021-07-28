d53881d07e943de0a1e34eb8350c04507997666f
hja <2684946387@qq.com>
2021-07-28 17:48:33 +0800

    test

--- ruoyi-ui/src/utils/ruoyi.js
+++ ruoyi-ui/src/utils/ruoyi.js
@@ -182,7 +182,7 @@ export function tansParams(params) {
 	let result = ''
 	Object.keys(params).forEach((key) => {
 		if (!Object.is(params[key], undefined) && !Object.is(params[key], null) && !Object.is(JSON.stringify(params[key]), '{}')) {
-			result += encodeURIComponent(key) + '=' + encodeURIComponent(params[key]) + '&'
+			// result += encodeURIComponent(key) + '=' + encodeURIComponent(params[key]) + '&'
 		}
 	})
 	return result
