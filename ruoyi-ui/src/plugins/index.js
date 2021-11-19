d11ffb8d0bc2bd3afb0f6e6a9df61a5758839afc
RuoYi <yzz_ivy@163.com>
2021-11-19 17:58:55 +0800

    新增tab对象简化页签操作

--- ruoyi-ui/src/plugins/index.js
+++ ruoyi-ui/src/plugins/index.js
@@ -1,9 +1,15 @@
+import tab from './tab'
+import auth from './auth'
 import cache from './cache'
 import modal from './modal'
 import download from './download'
 
 export default {
   install(Vue) {
+    // 页签操作
+    Vue.prototype.$tab = tab
+    // 认证对象
+    Vue.prototype.$auth = auth
     // 缓存对象
     Vue.prototype.$cache = cache
     // 模态框对象
