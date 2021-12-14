dc9104ee04a96de70f50fb011b9e4062a1e6190f
zhangbokang <540064991@qq.com>
2021-12-14 23:54:46 +0800

    添加ry-cloud建库语句

--- sql/ry_20210908.sql
+++ sql/ry_20210908.sql
@@ -1,4 +1,9 @@
+CREATE DATABASE  `ry-cloud` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
 SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+USE `ry-cloud`;
 
 -- ----------------------------
 -- 1、部门表
