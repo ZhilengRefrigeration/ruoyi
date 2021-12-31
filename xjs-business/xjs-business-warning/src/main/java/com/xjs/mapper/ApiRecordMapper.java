70c373401e84d7719592736f8ae52638acc9a8bb
xjs <1294405880@qq.com>
2021-12-31 22:12:04 +0800

    说明：1、预警服务已经成功启动      2、解决上次遗留大坑，mybatis扫不到mapper的原因：在resources中 . 是文件名的一部分，不会像package一样 . 会直接进入到子文件，所以mp扫描一直以为这是一个文件路径，配置的扫描规则对不上自然就扫不到mapper.xml文件了

---
+++ xjs-business/xjs-business-warning/src/main/java/com/xjs/mapper/ApiRecordMapper.java
@@ -0,0 +1,14 @@
+package com.xjs.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xjs.domain.ApiRecord;
+
+/**
+ * @author xiejs
+ * @desc  api预警mapper
+ * @create 2021-12-31
+ */
+public interface ApiRecordMapper extends BaseMapper<ApiRecord> {
+
+
+}
