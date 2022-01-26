204c458aadd37ee2c0195932e7bc0bdea2dde564
xjs <1294405880@qq.com>
2022-01-26 16:44:31 +0800

    1、主页实现一个仪表盘echarts

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -1,33 +1,128 @@
 <template>
   <div>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <div>
+          <el-card shadow="hover" class="card" ref="renderersChart">
 
+          </el-card>
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div>
+          <el-card shadow="hover" class="card">
 
+          </el-card>
+        </div>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <div>
+          <el-card shadow="hover" class="card">
+
+          </el-card>
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div>
+          <el-card shadow="hover" class="card">
+
+          </el-card>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 
+// 引入echarts
+var echarts = require('echarts/lib/echarts');
+require('echarts/lib/component/tooltip');
+require('echarts/lib/chart/gauge');
+
 
 export default {
   name: "Index",
   data() {
-    return {
-
-    };
+    return {};
   },
 
   created() {
-
   },
+
+  mounted() {
+    this.initRenderers();
+    },
+
 
   methods: {
+    initRenderers() {
+      var myDate = new Date();
+      var s = myDate.getSeconds();
+      let renderersChart = echarts.init(this.$refs.renderersChart.$el)
+      renderersChart.setOption({
+        tooltip: {
+          formatter: '单位：{a} <br/>当前 : {c}s'
+        },
+        series: [
+          {
+            name: '秒',
+            type: 'gauge',
+            progress: {
+              show: true
+            },
+            detail: {
+              valueAnimation: true,
+              formatter: '{value}'
+            },
+            data: [
+              {
+                value: s,
+                name: '单位：秒',
+              }
+            ],
+            min: 0,
+            max: 60,
+            splitNumber: 6,
+          }
+        ]
+      })
+
+      //定时获取秒数定时执行
+      setInterval(function () {
+        var myDate = new Date();
+        var s = myDate.getSeconds();
+        renderersChart.setOption({
+          series: [
+            {
+              data: [
+                {
+                  value: s
+                }
+              ]
+            },
+          ]
+        });
+      }, 1000);
+
+    },
 
   },
+
+
+
 
 };
 </script>
 
 <style scoped lang="scss">
+.card {
+  height: 410px;
+  margin: 0 auto;
+}
 
 
 </style>
