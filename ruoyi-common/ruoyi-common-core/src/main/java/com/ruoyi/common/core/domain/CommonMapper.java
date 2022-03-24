f8fd0a05e47b3b1a2b7e882becee64c5a11fdae6
Layne Cai <15506906353@163.com>
2022-03-24 17:40:48 +0800

    mid:准备开始改造mybatis->mybatisplus 1.准备基本类,基本服务

---
+++ ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/domain/CommonMapper.java
@@ -0,0 +1,13 @@
+package com.ruoyi.common.core.domain;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 通用mapper 数据层
+ * 
+ * @author pinn
+ */
+public interface CommonMapper<T> extends BaseMapper<T>
+{
+
+}
--- ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SysConfigServiceImpl.java
+++ ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SysConfigServiceImpl.java
@@ -2,7 +2,12 @@ package com.ruoyi.system.service.impl;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import javax.annotation.PostConstruct;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ruoyi.common.core.constant.Constants;
@@ -203,5 +208,50 @@ public class SysConfigServiceImpl implements ISysConfigService
     private String getCacheKey(String configKey)
     {
         return Constants.SYS_CONFIG_KEY + configKey;
+    }
+
+    @Override
+    public boolean saveBatch(Collection<SysConfig> entityList, int batchSize) {
+        return false;
+    }
+
+    @Override
+    public boolean saveOrUpdateBatch(Collection<SysConfig> entityList, int batchSize) {
+        return false;
+    }
+
+    @Override
+    public boolean updateBatchById(Collection<SysConfig> entityList, int batchSize) {
+        return false;
+    }
+
+    @Override
+    public boolean saveOrUpdate(SysConfig entity) {
+        return false;
+    }
+
+    @Override
+    public SysConfig getOne(Wrapper<SysConfig> queryWrapper, boolean throwEx) {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getMap(Wrapper<SysConfig> queryWrapper) {
+        return null;
+    }
+
+    @Override
+    public <V> V getObj(Wrapper<SysConfig> queryWrapper, Function<? super Object, V> mapper) {
+        return null;
+    }
+
+    @Override
+    public BaseMapper<SysConfig> getBaseMapper() {
+        return null;
+    }
+
+    @Override
+    public Class<SysConfig> getEntityClass() {
+        return null;
     }
 }
