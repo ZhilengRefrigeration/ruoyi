f6ecaf65327d4d0c6624c48930bf95688ba5193a
wuyibo <771227828@qq.com>
2022-08-30 18:22:02 +0800

    修改标题，logo

--- ruoyi-ui/src/layout/components/Sidebar/Logo.vue
+++ ruoyi-ui/src/layout/components/Sidebar/Logo.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script>
-import logoImg from '@/assets/logo/logo.png'
+import logoImg from '@/assets/logo/logo.jpg'
 import variables from '@/assets/styles/variables.scss'
 
 export default {
@@ -35,7 +35,7 @@ export default {
   },
   data() {
     return {
-      title: '若依管理系统',
+      title: '篮球Zone后台管理系统',
       logo: logoImg
     }
   }
