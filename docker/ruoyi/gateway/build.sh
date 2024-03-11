1f62b5c3059c7e53f3d11aa9f21670f991e57d1c
AlanScipio <as@git.com>
2024-03-11 14:31:50 +0800

    完善docker相关部署

---
+++ docker/ruoyi/gateway/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define the image name as a variable for easy modification
+IMAGE_NAME="ryas-gateway"
+
+# Prompt the user to enter a version number (tag) for the image
+echo "Please enter the image version number (tag):"
+read TAG
+
+# Check if the version number has been entered
+if [[ -z "$TAG" ]]; then
+    echo "The image tag cannot be empty !"
+    exit 1
+fi
+
+DOCKERFILE_DIR='.'
+
+docker buildx build -t $IMAGE_NAME:$TAG $DOCKERFILE_DIR
+
+echo
+echo "================================================"
+echo "The details of the built image are as follows:"
+echo
+docker images | grep "$IMAGE_NAME.*$TAG"
+
